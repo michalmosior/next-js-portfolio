1d59aabd11a97e7bda720b1e7a71ffaf296efc69
michalmosior <michal.mosior@gmail.com>
2023-09-16 14:12:23 +0200

    fix fetching data

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,7 +2,9 @@ import DecorationSpan from '../UI/DecorationSpan';
 import Image from 'next/image';
 
 const fetchSkills = async () => {
-	const response = await fetch(`https://michalmosior.vercel.app/api/skills`);
+	const response = await fetch(
+		`https://michalmosior-nqkwb6joj-michalmosior-gmailcom.vercel.app/api/skills`
+	);
 	const skills = await response.json();
 	if (!response.ok) {
 		throw new Error('Failed to fetch skills');
