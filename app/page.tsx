a5d7d61ec2e30b8093cb69f2889eb338bda02692
michalmosior <michal.mosior@gmail.com>
2023-09-18 11:39:10 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,28 @@ import Projects from '@/components/Projects/Projects';
 import About from '../components/About/About';
 import Header from '../components/Header/Header';
 
+const fetchSkills = async () => {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/skills`,
+		{
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' },
+		}
+	);
+	const skills = await response.json();
+	if (!response.ok) {
+		throw new Error('Failed to fetch skills');
+	}
+	return skills;
+};
+
 const Home = async () => {
+	const allSkills = await fetchSkills();
 	return (
 		<>
+			{allSkills.map((skill: any) => {
+				return <p>{skill.title}</p>;
+			})}
 			<Header />
 			{/*<About />
 			<Projects />*/}
